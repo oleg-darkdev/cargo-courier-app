638b8e73a56dc72810e2b3d6ed5c111c9b8efa46
DarkDev <darkdev_brand@riseup.net>
2023-09-07 19:31:30 +0300

    default routing

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Nav, Hero, Footer, WhatWeDo, AboutUs, Testimational, OurPartners } from '../lib/widgets';
+</script>
+
+	<Nav />
+
+	<Hero />
+	<OurPartners />
+
+	<WhatWeDo />
+
+	<AboutUs />
+	<Testimational />
+
+	<Footer />
