30eee27e2c8172bcb0013b827799f32ee7fe50b1
DarkDev <darkdev_brand@riseup.net>
2023-09-07 19:30:05 +0300

    upd api

---
+++ src/lib/widgets/index.js
@@ -0,0 +1,9 @@
+import Nav from './ui/landing/Nav.svelte';
+import Hero from './ui/landing/Hero.svelte';
+import Footer from './ui/landing/Footer.svelte';
+import WhatWeDo from './ui/landing/WhatWeDo.svelte';
+import AboutUs from './ui/landing/AboutUs.svelte';
+import Testimational from './ui/landing/Testimational.svelte';
+import OurPartners from './ui/landing/OurPartners.svelte';
+
+export { Nav, Hero, Footer, WhatWeDo, AboutUs, Testimational, OurPartners };
