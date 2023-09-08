8729f007f12f1744089d1baad54ff54050285cb4
DarkDev <darkdev_brand@riseup.net>
2023-09-08 12:09:20 +0300

    upd colours scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,15 +14,14 @@ module.exports = {
 		themes: [
 			{
 				mytheme: {
-					primary: '#661AE6',
-					secondary: '#D926AA',
+					primary: '#f44336',
+					secondary: '#331b3b',
 					accent: '#1FB2A5',
 					neutral: '#191D24',
 					info: '#3ABFF8',
 					success: '#36D399',
 					warning: '#FBBD23',
-					error: '#F87272',
-
+					error: '#F87272'
 				}
 			}
 		]
