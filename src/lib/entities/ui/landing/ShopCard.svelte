9e35233bff34fa5a991119a6c6ab47acf5929128
DarkDev <darkdev_brand@riseup.net>
2023-10-31 15:45:22 +0200

    alfa v.

---
+++ src/lib/entities/ui/landing/ShopCard.svelte
@@ -0,0 +1,94 @@
+<script>
+	export let productData;
+</script>
+
+<div
+	class="mx-2 w-full max-w-sm  rounded-tl-[40px] rounded-br-[40px] bg-secondary shadow dark:border-gray-700 "
+>
+	<a href="#">
+		<img class="rounded-tl-[80px] p-8" src={productData.img} alt={productData.title} />
+	</a>
+	<div class="px-5 pb-5">
+		<a href="#">
+			<h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+				{productData.title}
+			</h5>
+		</a>
+
+		<div class="mb-4">
+			{#each productData.desc as desc}
+				<p>{desc}</p>
+			{/each}
+		</div>
+
+		<!-- <div class="mt-2.5 mb-5 flex items-center">
+			<svg
+				class="mr-1 h-4 w-4 text-yellow-300"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 22 20"
+			>
+				<path
+					d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+				/>
+			</svg>
+			<svg
+				class="mr-1 h-4 w-4 text-yellow-300"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 22 20"
+			>
+				<path
+					d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+				/>
+			</svg>
+			<svg
+				class="mr-1 h-4 w-4 text-yellow-300"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 22 20"
+			>
+				<path
+					d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+				/>
+			</svg>
+			<svg
+				class="mr-1 h-4 w-4 text-yellow-300"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 22 20"
+			>
+				<path
+					d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+				/>
+			</svg>
+			<svg
+				class="h-4 w-4 text-gray-200 dark:text-gray-600"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 22 20"
+			>
+				<path
+					d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+				/>
+			</svg>
+			<span
+				class="mr-2 ml-3 rounded bg-blue-100 px-2.5 py-0.5 text-xs font-semibold text-blue-800 dark:bg-blue-200 dark:text-blue-800"
+				>5.0</span
+			>
+		</div> -->
+		<div class="flex items-center justify-between">
+			<span class="text-3xl font-bold text-white">{productData.price}</span>
+			<a
+				href="#"
+				class="hover:bg-primary-700 rounded-lg bg-primary px-5 py-2.5 text-center text-sm font-medium text-secondary focus:ring-primary"
+				>Add to cart</a
+			>
+		</div>
+	</div>
+</div>
